b8abed60b76b64f6149292e0ae39889b660623d1
Valdo <vlorian22@GMAIL.COM>
2020-11-02 21:58:27 -0600

    started with the file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,11 @@
-!
+// Jquery Documentation
+
+$(document).ready(()=>{
+
+    // Create input box for the search input
+    $('.btn').on('click',()=>
+    {
+      var srchInput= $('#searchbox').val();
+    
+    });
+});
