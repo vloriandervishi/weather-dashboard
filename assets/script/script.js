84137d4d346163e8fa3f9200a4b215f3eee6a6fb
Valdo <vlorian22@GMAIL.COM>
2020-11-08 10:06:47 -0600

    added city names, temperature,humidity

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,37 +1,40 @@
 
 
-// Jquery Documentation
-let headerEl = document.createElement('header');
-// create class along the header
-headerEl.setAttribute("class", "hd-1");
-document.body.appendChild(headerEl);
-// create h1 tag inside the header 
-let h1El = document.createElement('h1');
-headerEl.appendChild(h1El);
-h1El.innerText = " Weather Dashboard";
-h1El.style.textAlign = "center";
-headerEl.style.backgroundColor = "lightblue";
-h1El.style.color = 'white';
-headerEl.style.padding = "50px";
-let container = document.createElement('div');
-container.setAttribute('class', 'row');
-document.body.appendChild(container);
-let input = document.createElement('input');
-input.setAttribute('id', 'search-box');
-input.setAttribute('placeholder', 'enter city name');
-container.appendChild(input);
-let labeler = document.createElement('label');
-labeler.setAttribute('id', 'search-box')
-container.appendChild(labeler);
-let searchButton = document.createElement('button');
-searchButton.setAttribute('class', 'srchBtn');
-let iBtn=document.createElement('i');
-iBtn.setAttribute('class','fa fa-search');
-searchButton.setAttribute('type','submit');
-searchButton.appendChild(iBtn);
-container.appendChild(searchButton);
 
-$(document).ready(() => {
-  // Create input box for the search input
+$(document).ready(function () {
+  $("button").on('click', function () {
+    var input = document.querySelector('#sx').value;
+    var apiUrlKey = `http://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=768d6bd4bf42c06b0464c112e838748f`
+    // Converts the response to JSON
+    async function getWeatherInfo() {
+      let response = await fetch(apiUrlKey);
+      let data = await response.json();
+      return data;
+    }
+    getWeatherInfo().then(data => {
+      console.log(data.list[0].wind);
+      // Search city name
+      var iconList = data.list[0].weather[0].icon;
+      var weatherIcon = $('<img id="weatherIcon">');
+      weatherIcon.attr('src', `http://openweathermap.org/img/wn/${iconList}@2x.png`);
+      $(".name").append('Temperature: '+ data.city.name);
+      
+      $(".temp").append(Math.floor(data.list[0].main.temp - 273.15) * 1.80 + 32 + '℉ ');
+      $(".humidity").prepend('Humidity: ')
+      $(".humidity").append(data.list[0].main.humidity + '%');
 
+      $(".wind").append(data.list[0].wind.speed + ' mph');
+
+      $(".uv-index").append();
+      weatherIcon.appendTo('.name');    
+
+    });
+    function clearField() {
+      $(".name").empty();
+      $(".humidity").empty();
+      $(".wind").empty();
+      $(".uv-index").empty();
+    }
+    clearField();
+  });
 });
